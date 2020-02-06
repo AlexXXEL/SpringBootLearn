a4206b09ad7fa286c575d04a740111ab72283b0f
alex <jiaqianfang18@gmail.com>
2020-02-06 21:36:40 +0800

    afterlogset

---
+++ src/main/java/com/learn/springbootlearn/SpringBootLearn.java
@@ -0,0 +1,13 @@
+package com.learn.springbootlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLearn {
+    public static void main(String[] args){
+        SpringApplication.run(SpringBootLearn.class,args);
+    }
+
+
+}
